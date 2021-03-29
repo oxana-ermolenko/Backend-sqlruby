19279caccd568dd1fac3464b3a7e6daf25fa6145
oxana-ermolenko <vagrant@vagrant.vm>
2021-03-29 00:16:58 -0700

    corrected minutes and added seconds

--- years_to_hours.rb
+++ years_to_hours.rb
@@ -9,5 +9,12 @@ puts "That's #{hours} hours."
 puts "Enter a number of decades"  
 decades = gets.chomp # this returns a string  
 decades = decades.to_i # this converts a string to an integer  
-minutes = decades * 10 * 24 * 60 
+minutes = decades * 10 * 365 * 24 * 60 
 puts "That's #{minutes} minutes."
+
+# part 3
+puts "Enter your age"  
+age = gets.chomp # this returns a string  
+age = age.to_i # this converts a string to an integer  
+seconds = age * 365 * 24 * 60 * 60
+puts "That's #{seconds} seconds."
